43cb00fc8c1a4d0695c1c7fc6ee02fca5e9f1232
Markkus Millend <markkusmillend@icloud.com>
2020-11-03 00:13:50 +0200

    Rename components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld v-if="contract" msg="Welcome to Your Vue.js App"/>
+    <GuestBook v-if="contract" msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import GuestBook from './components/GuestBook.vue';
 import { mapGetters } from 'vuex';
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    GuestBook,
   },
   computed: {
     ...mapGetters(['contract'])
