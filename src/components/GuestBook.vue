34cd46e1698d288ffb0ba94e59e518b941a992a5
Markkus Millend <markkusmillend@icloud.com>
2020-11-03 02:16:06 +0200

    Add message posting

--- src/components/GuestBook.vue
+++ src/components/GuestBook.vue
@@ -8,15 +8,15 @@
     </header>
     <template v-if="currentUser">
       <form @submit="sendMessage">
-        <fieldset id="fieldset">
+        <fieldset id="fieldset" :disabled="submitting">
           <p>Sign the guest book, {{ currentUser.accountId }}!</p>
           <p class="highlight">
             <label for="message">Message:</label>
-            <input autocomplete="off" autofocus id="message" required/>
+            <input autocomplete="off" v-model="message" autofocus id="message" required/>
           </p>
           <p>
             <label for="donation"> Donation (optional):</label>
-            <input autocomplete="off" :value="suggestedDonation"
+            <input autocomplete="off" v-model="donation"
                    id="donation" :max="maxDonation" min="0" step="0.01" type="number"/>
             <span title="NEAR Tokens">Ⓝ</span>
           </p>
@@ -66,8 +66,11 @@ export default {
   data() {
     return {
       messages: [],
-      suggestedDonation: 0,
-      maxDonation: 0 // By default, set as 0.
+      submitting: false,
+      donation: 0,
+      message: null,
+      maxDonation: 0, // By default, set as 0.
+      gas: Big(3).times(10 ** 13).toFixed()
     };
   },
   async mounted() {
@@ -87,8 +90,27 @@ export default {
       this.wallet.signOut();
       window.location.replace(window.location.origin + window.location.pathname)
     },
-    sendMessage() {
-
+    async sendMessage(e) {
+      e.preventDefault();
+      this.submitting = true;
+      // Asynchronously post the message and get all of the messages again.
+      try {
+        await this.contract.addMessage(
+          { text: this.message },
+          this.gas,
+          Big(this.donation || '0').times(10 ** 24).toFixed()
+        );
+      } catch (e) {
+        console.log(e);
+        alert('Something went wrong! Check the console.');
+      } finally {
+        const messages = await this.contract.getMessages();
+        this.messages = messages.reverse();
+      }
+      // Reset values.
+      this.message = null;
+      this.donation = 0;
+      this.submitting = false;
     }
   }
 };
